eca0736d69a906cca357b31ac91e53ff39d41e44
Diego Eguez <deguez07@gmail.com>
2026-03-26 13:22:05 -0500

    Add menu bar with "New backup task" action

--- composeApp/src/jvmMain/kotlin/com/muna/filebackups/main.kt
+++ composeApp/src/jvmMain/kotlin/com/muna/filebackups/main.kt
@@ -1,5 +1,8 @@
 package com.muna.filebackups
 
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -8,6 +11,15 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "filebackups",
     ) {
+        MenuBar {
+            Menu("Actions") {
+                Item(
+                    text = "New backup task",
+                    shortcut = KeyShortcut(Key.N, meta = System.getProperty("os.name").contains("Mac", ignoreCase = true), ctrl = !System.getProperty("os.name").contains("Mac", ignoreCase = true)),
+                    onClick = { println("New backup task initiated") },
+                )
+            }
+        }
         App()
     }
 }
